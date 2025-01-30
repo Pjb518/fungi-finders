9172f29c8c4f5bdf93507c7e810a8b2e45d112a4
Phil <me@phillip.best>
2025-01-30 15:46:23 +0000

    Adds markup for a second page. Adds mushroom data to allow for dynamically generating mushroom information cards on the second page.

--- main.js
+++ main.js
@@ -1,13 +1,88 @@
-function parseBooleanString(str) {
-    return /^true$/i.test(str);
+function createMushroomCards(mushroomData) {
+	return mushroomData.map((mushroom) => {
+		const title = document.createElement("h3");
+		title.classList.add(["mushroom-card__title"]);
+		title.innerText = mushroom.name;
+
+		const tagList = document.createElement("ul");
+		tagList.classList.add(["mushroom-card__tag-list"]);
+		tagList.setAttribute("role", "list");
+		tagList.append(...createMushroomTags(mushroom));
+
+		const description = document.createElement("p");
+		description.innerText = mushroom.description;
+
+		const notes = document.createElement("strong");
+		notes.classList.add(["mushroom-card__notes"]);
+		notes.innerHTML = `<span>Important information:</span> ${mushroom.notes}`;
+
+		const card = document.createElement("article");
+		card.classList.add(["card"]);
+		card.append(title, tagList, description, notes);
+
+		return card;
+	});
 }
 
-globalThis.addEventListener("DOMContentLoaded", () => {
-    const navigationButton = document.querySelector("[aria-controls='primary-nav']");
-    const primaryNavigation = document.getElementById("primary-nav");
+function createMushroomTags({ isToxic, harvestSeason }) {
+	const mushroomToxicity = isToxic ? "toxic" : "edible";
 
-    navigationButton.addEventListener("click", function (event) {
-        const currentExpandedState = parseBooleanString(this.getAttribute("aria-expanded"));
-        this.setAttribute("aria-expanded", !currentExpandedState);
-    });
+	const toxicityTag = document.createElement("li");
+	toxicityTag.setAttribute("data-toxicity", mushroomToxicity);
+	toxicityTag.innerText = mushroomToxicity;
+
+	const seasonTag = document.createElement("li");
+	seasonTag.setAttribute("data-season", harvestSeason);
+	seasonTag.innerText = harvestSeason;
+
+	return [toxicityTag, seasonTag];
+}
+
+function parseBooleanString(str) {
+	return /^true$/i.test(str);
+}
+
+globalThis.addEventListener("DOMContentLoaded", async () => {
+	const response = await fetch("./data/mushrooms.json");
+	const mushroomData = await response.json();
+	const mushroomCards = createMushroomCards(mushroomData);
+
+	const mushroomCardsWrapper = document.getElementById("mushroom-guide-cards");
+	mushroomCardsWrapper.append(...mushroomCards);
+
+	// <article class="mushroom-card"
+	// 	<h3>Chanterelle</h3>
+
+	// 	<ul class="mushroom-card__tag-list" role="list">
+	// 		<li class="mushroom-card__tag" data-toxicity="edible">
+	// 			<span class="visually-hidden">This mushroom is </span> edible
+	// 		</li>
+
+	// 		<li class="mushroom-card__tag" data-season="summer">
+	// 			<span class="visually-hidden">The best time to harvest this mushroom is </span> summer
+	// 		</li>
+	// 	</ul>
+
+	// 	<p>
+	// 		Golden-yellow, funnel-shaped mushroom with false gills
+	// 	</p>
+
+	// 	<strong class="mushroom-card__warnings">
+	// 		<span>Important notes:</span> Has toxic lookalikes—learn proper identification
+	// 	</strong>
+	// </article>
+});
+
+globalThis.addEventListener("DOMContentLoaded", () => {
+	const navigationButton = document.querySelector(
+		"[aria-controls='primary-nav']",
+	);
+
+	navigationButton.addEventListener("click", function () {
+		const currentExpandedState = parseBooleanString(
+			this.getAttribute("aria-expanded"),
+		);
+
+		this.setAttribute("aria-expanded", !currentExpandedState);
+	});
 });
